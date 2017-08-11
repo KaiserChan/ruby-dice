e898618d5a099f2942dabb052e522dad70cca7c2
KaiserChan <chankaiserkk@gmail.com>
2017-08-11 14:19:47 -0400

    Created doubles.rb - Completed exercise

---
+++ doubles.rb
@@ -0,0 +1,13 @@
+dice_1 = rand(6) + 1
+dice_2 = rand(6) + 1
+
+puts "You rolled #{dice_1} and #{dice_2}."
+
+if dice_1 == dice_2
+  puts "You rolled a #{dice_1} and a #{dice_2} ..."
+  puts "Its a Doubles!"
+else
+  puts "You rolled a #{dice_1} and a #{dice_2} ..."
+end
+
+puts "You total is #{dice_1 + dice_2}."
